53fe7ee21317c146bf494686a9bfe77d3c2fd9b6
josuebaez61@gmail.com <josuebaez61@gmail.com>
2024-03-20 21:12:37 -0300

    NgStyle y NgClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'clase-04-no-standalone';
+
+  hasError = true;
+
+  isDisabled = true;
+
+  alternarError() {
+    this.hasError = !this.hasError;
+  }
 }
